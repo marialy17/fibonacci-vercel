46f38ccab7cd4e56b0c4b949da9631ecc33fa636
marialy17 <marialy.diaz@g-tech.io>
2025-06-10 13:57:18 -0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'API Fibonacci',
+  description: 'API para generar secuencia Fibonacci',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
